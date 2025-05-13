ababf4b7a534c14dc66c4f879703bca9aeebfc54
lroussel <lroussel@student.42perpignan.fr>
2025-05-13 17:35:59 +0200

    fix

--- src/objects/factory.c
+++ src/objects/factory.c
@@ -6,7 +6,7 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 09:45:41 by lroussel          #+#    #+#             */
-/*   Updated: 2025/05/13 17:26:39 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/05/13 17:35:17 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,11 @@ int	register_object(void *object)
 int	register_light(t_light *light)
 {
 	char		*id;
-	int			len;
 
 	if (!light)
 		return (0);
 	id = light->id;
-	len = ft_strlen(id);
-	if (ft_strncmp(LIGHT_ID, id, len) != 0)
+	if (ft_strncmp(LIGHT_ID, id, ft_strlen(id)) != 0)
 	{
 		free(light);
 		return (0);
@@ -54,13 +52,11 @@ int	set_ambiant(t_ambiant *ambiant)
 {
 	t_minirt	*mrt;
 	char		*id;
-	int			len;
 
 	if (!ambiant)
 		return (0);
 	id = ambiant->id;
-	len = ft_strlen(id);
-	if (ft_strncmp(AMBIANT_ID, id, len) != 0)
+	if (ft_strncmp(AMBIANT_ID, id, ft_strlen(id)) != 0)
 	{
 		free(ambiant);
 		return (0);
@@ -75,13 +71,11 @@ int	set_camera(t_camera *camera)
 {
 	t_minirt	*mrt;
 	char		*id;
-	int			len;
 
 	if (!camera)
 		return (0);
 	id = camera->id;
-	len = ft_strlen(id);
-	if (ft_strncmp(CAMERA_ID, id, len) != 0)
+	if (ft_strncmp(CAMERA_ID, id, ft_strlen(id)) != 0)
 	{
 		free(camera);
 		return (0);
