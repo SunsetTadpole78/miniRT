c949afaaeb0cbe57e086bb83d6a58542347d8412
lroussel <lroussel@student.42perpignan.fr>
2025-05-14 18:24:09 +0200

    fix all && start ambiant parsing

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 18:30:37 by lroussel          #+#    #+#             */
-/*   Updated: 2025/05/14 18:18:30 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/05/14 18:23:09 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 
 # include "libft.h"
 # include "mlx.h"
-# include <fcntl.h>
 
 // Macros for Type Objects
 
