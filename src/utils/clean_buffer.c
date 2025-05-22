cd782aeceba426c4dac4b1b2d588dc4ad083ec9f
justJuste <jules.longin1@gmail.com>
2025-05-22 13:42:11 +0200

    conditional_jump

--- src/utils/clean_buffer.c
+++ src/utils/clean_buffer.c
@@ -16,12 +16,9 @@ void	clean_buffer(t_minirt *mrt)
 {
 	int		i;
 	int		scale;
-	t_mlx	*mlx;
 
 	i = 0;
-	mlx = mrt->mlx;
 	scale = WIN_HEIGHT * WIN_WIDTH;
-	ft_bzero(mlx->data, WIN_HEIGHT * mlx->ll);
 	while (i < scale)
 	{
 		mrt->buffer[i] = (t_fvector3){0.0f, 0.0f, 0.0f};
