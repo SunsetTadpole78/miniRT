3511d5e0c8be993deb27002cc49d8acab8672ff6
lroussel <lroussel@student.42perpignan.fr>
2025-06-20 13:41:15 +0200

    fix render.c

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By:                                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created:   by Juste                               #+#    #+#             */
-/*   Updated: 2025/06/20 13:36:00 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/06/20 13:40:48 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,53 @@
 
 /* ------------------------------- PROTOTYPE -------------------------------- */
 static inline void		*render_part(void *value);
+static inline void		init_threads(t_minirt *mrt);
+static inline void		fill_image(t_thread_data *data);
+static inline void		wait_workers(t_minirt *mrt, int cores);
 /* -------------------------------------------------------------------------- */
 
-static inline void	wait_workers(t_minirt *mrt, int cores)
+void	render_scene(t_minirt *mrt)
 {
-	int	i;
+	int				cores;
+	t_thread_data	*datas;
+	t_mlx			*mlx;
+	int				i;
 
+	init_threads(mrt);
+	cores = mrt->cores;
+	datas = mrt->threads_datas;
+	fill_image(&datas[cores - 1]);
+	mlx = mrt->mlx;
+	mlx->count++;
+	wait_workers(mrt, cores);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->image.ptr, 0, 0);
 	i = 0;
 	while (i < (cores - 1))
 	{
-		sem_wait(&mrt->workers_sem);
+		sem_post(&mrt->workers_sem);
 		i++;
 	}
 }
 
-void	init_threads(t_minirt *mrt)
+static inline void	*render_part(void *value)
+{
+	t_thread_data	*data;
+
+	data = (t_thread_data *)value;
+	while (!can_exit(data->mrt))
+	{
+		sem_wait(&data->update_sem);
+		sem_post(&data->update_sem);
+		if (can_exit(data->mrt))
+			return (NULL);
+		fill_image(data);
+		sem_post(&data->mrt->workers_sem);
+		sem_wait(&data->update_sem);
+	}
+	return (NULL);
+}
+
+static inline void	init_threads(t_minirt *mrt)
 {
 	int				i;
 	int				cores;
@@ -73,43 +105,14 @@ static inline void	fill_image(t_thread_data *data)
 	}
 }
 
-void	render_scene(t_minirt *mrt)
+static inline void	wait_workers(t_minirt *mrt, int cores)
 {
-	int				cores;
-	t_thread_data	*datas;
-	t_mlx			*mlx;
-	int				i;
+	int	i;
 
-	init_threads(mrt);
-	cores = mrt->cores;
-	datas = mrt->threads_datas;
-	fill_image(&datas[cores - 1]);
-	mlx = mrt->mlx;
-	mlx->count++;
-	wait_workers(mrt, cores);
-	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->image.ptr, 0, 0);
 	i = 0;
 	while (i < (cores - 1))
 	{
-		sem_post(&mrt->workers_sem);
+		sem_wait(&mrt->workers_sem);
 		i++;
 	}
-}
-
-static inline void	*render_part(void *value)
-{
-	t_thread_data	*data;
-
-	data = (t_thread_data *)value;
-	while (!can_exit(data->mrt))
-	{
-		sem_wait(&data->update_sem);
-		sem_post(&data->update_sem);
-		if (can_exit(data->mrt))
-			return (NULL);
-		fill_image(data);
-		sem_post(&data->mrt->workers_sem);
-		sem_wait(&data->update_sem);
-	}
-	return (NULL);
 }
