a2f46ba594790353369a3d02550ad1ba289931cd
lroussel <lroussel@student.42perpignan.fr>
2025-06-20 15:41:48 +0200

    fix action

--- scripts/check_valgrind_parsing.sh
+++ scripts/check_valgrind_parsing.sh
@@ -5,10 +5,9 @@ MAP_DIR="./maps/parsing"
 for file_path in "$MAP_DIR"/*.rt; do
 	file_name=$(basename "$file_path")
 
-	[[ "$file_name" == "empty.rt" ]] && continue
 	./scripts/check_valgrind.sh $file_path 0
-	exit_code=$(echo "$?")
-	if [[ ! $? -eq 0 ]]; then
-		exit 1
+	exit_code=$?
+	if [[ $exit_code -ne 0 ]]; then
+	    exit 1
 	fi
 done
