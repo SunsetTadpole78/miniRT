b6d41d8c7a97d792051a762fc632c16d1c611d2c
justJuste <jules.longin1@gmail.com>
2025-06-27 23:42:05 +0200

    add earth

--- src/objects/sphere/render.c
+++ src/objects/sphere/render.c
@@ -14,11 +14,12 @@
 #include "errors.h"
 
 /* ------------------------------- PROTOTYPE -------------------------------- */
-static inline int		init_sphere(t_ray *ray, t_hit_data *hit,
-							t_sphere *sphere);
-static inline t_rgb	get_base_color(t_pattern pattern, t_hit_data hit,
-							int inside);
-static inline t_rgb	display_texture(t_mlx_image texture, t_hit_data hit);
+static inline int			init_sphere(t_ray *ray, t_hit_data *hit,
+								t_sphere *sphere);
+static inline t_rgb		get_base_color(t_pattern pattern, t_hit_data hit,
+								int inside);
+static inline t_rgb		display_texture(t_mlx_image texture, float u, float v);
+static inline t_fvector3	bump_mapping(t_mlx_image bump, t_hit_data hit);
 /* -------------------------------------------------------------------------- */
 
 void	apply_lights_sphere(t_minirt *mrt, t_ray *ray, t_object *object,
@@ -32,6 +33,8 @@ void	apply_lights_sphere(t_minirt *mrt, t_ray *ray, t_object *object,
 
 	sphere = (t_sphere *)object;
 	inside = init_sphere(ray, &hit, sphere);
+	if (sphere->pattern.bump_path)
+		hit.normal = bump_mapping(sphere->pattern.bump, hit);
 	base = get_base_color(sphere->pattern, hit, inside);
 	if (base.r != 0 || base.g != 0 || base.b != 0)
 		ray->color = apply_lights_modifier(
@@ -60,8 +63,8 @@ static inline int	init_sphere(t_ray *ray, t_hit_data *hit, t_sphere *sphere)
 	hit->position = sphere->position;
 	hit->diff = (t_fvector3){0.0f, 0.0f, 0.0f};
 	hit->proj = (t_fvector3){0.0f, 0.0f, 0.0f};
-	hit->u = 0.0f;
-	hit->v = 0.0f;
+	hit->u = 0.5f + atan2f(hit->normal.z, hit->normal.x) / (2.0f * M_PI);
+	hit->v = 0.5f - asinf(hit->normal.y) / M_PI;
 	hit->h = 0.0f;
 	inside = ft_fvector3_length(ft_fvector3_diff(ray->origin,
 				sphere->position)) < sphere->radius;
@@ -78,42 +81,42 @@ static inline t_rgb	get_base_color(t_pattern pattern, t_hit_data hit,
 		return (pattern.main_color);
 	if (inside)
 		hit.normal = ft_fvector3_scale(hit.normal, -1);
-	hit.u = 0.5f + atan2f(hit.normal.z, hit.normal.x) / (2.0f * M_PI);
-	hit.v = 0.5f - asinf(hit.normal.y) / M_PI;
 	if (pattern.id == 'c'
 		&& (int)((floorf(hit.u * 10.0f)) + (floorf(hit.v * 10.0f))) & 1)
 		return (pattern.secondary_color);
 	if (pattern.path)
-		return (display_texture(pattern.texture, hit));
+		return (display_texture(pattern.texture, hit.u, hit.v));
 	return (pattern.main_color);
 }
 
-static inline t_rgb	display_texture(t_mlx_image texture, t_hit_data hit)
+static inline t_rgb	display_texture(t_mlx_image texture, float u, float v)
 {
 	return (mlx_pixel_to_rgb(texture,
-			(int)(hit.u * texture.width) % texture.width,
-		(int)(hit.v * texture.height) % texture.height));
+			(int)(u * texture.width) % texture.width,
+		(int)(v * texture.height) % texture.height));
 }
 
-/*
-static inline t_fvector3	bump_mapping(t_mlx_image bump, t_fvector3 normal,
-	float u, float v)
+static inline t_fvector3	bump_mapping(t_mlx_image bump, t_hit_data hit)
 {
 	t_rgb		c;
 	float		du;
 	float		dv;
-	float		hC;
+	float		hc;
 	t_fvector3	t;
 
-	c = display_texture(bump, u, v);
-	hC = (c.r + c.g + c.b) / 3.0f;
-	c = display_texture(bump, u + 1.0f / (float)bump.width, v);
-	du = ((c.r + c.g + c.b) / 3.0f - hC) * 0.8f;
-	c = display_texture(bump, u, v + 1.0f / (float)bump.height);
-	dv = ((c.r + c.g + c.b) / 3.0f - hC) * 0.8f;
-	t = ft_fnormalize(ft_fcross_product((t_fvector3){0.0f, 1.0f, 0.0f}, normal));
-	return (ft_fnormalize(ft_fvector3_sum(normal,
-		ft_fvector3_sum(ft_fvector3_scale(t, du),
-		ft_fvector3_scale(ft_fnormalize(ft_fcross_product(normal, t)), dv)))));
+	c = display_texture(bump, hit.u, hit.v);
+	hc = (c.r + c.g + c.b) / 3.0f;
+	c = display_texture(bump, hit.u + 1.0f / (float)bump.width, hit.v);
+	du = ((c.r + c.g + c.b) / 3.0f - hc) * 1.0f;
+	c = display_texture(bump, hit.u, hit.v + 1.0f / (float)bump.height);
+	dv = ((c.r + c.g + c.b) / 3.0f - hc) * 1.0f;
+	if (fabsf(hit.normal.y) > 0.9f)
+		t = ft_fnormalize(
+				ft_fcross_product((t_fvector3){1.0f, 0.0f, 0.0f}, hit.normal));
+	else
+		t = ft_fnormalize(
+				ft_fcross_product((t_fvector3){0.0f, 1.0f, 0.0f}, hit.normal));
+	return (ft_fnormalize(ft_fvector3_sum(hit.normal, ft_fvector3_sum(
+					ft_fvector3_scale(t, du), ft_fvector3_scale(ft_fnormalize(
+							ft_fcross_product(hit.normal, t)), dv)))));
 }
-*/
